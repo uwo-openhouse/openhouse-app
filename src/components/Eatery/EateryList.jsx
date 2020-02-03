62e9dc3297acacf4c6ddc8c66cabb654b26afbaa
robert2468 <robertpark@Roberts-MacBook-Pro.local>
2020-02-02 20:49:14 -0500

    Prototype of the eatery page based on schedule.

---
+++ src/components/Eatery/EateryList.jsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    SafeAreaView,
+    SectionList,
+    View
+} from 'react-native';
+import * as PropTypes from 'prop-types';
+import moment from "moment";
+import {formatTime} from "../../service";
+import EateryItem from "../../containers/Eatery/EateryItem";
+
+const timeCompare = (time1, time2) => {
+    const parsedTime1 = moment(time1, 'H:m');
+    const parsedTime2 = moment(time2, 'H:m');
+
+    if (parsedTime1.isBefore(parsedTime2)){
+        return -1;
+    }
+    if (parsedTime1.isAfter(parsedTime2)){
+        return 1;
+    }
+    return 0;
+};
+
+const isOpen = (eatery) => {
+    const parsedOpenTime = moment(eatery.openTime, 'H:m');
+    const parsedCloseTime = moment(eatery.closeTime, 'H:m');
+    const parsedCurrentTime = moment(new Date().getDate(), 'H:m');
+
+    if (parsedOpenTime.isAfter(parsedCurrentTime) && parsedCloseTime.isBefore(parsedCurrentTime)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+const sortEateries = (a, b) => {
+    const openA = isOpen(a);
+    const openB = isOpen(b);
+
+    if (openA == openB) {
+        return a.name.localeCompare(b.name);
+    }
+    else if (openA) {
+        return -1;
+    }
+    else if (openB) {
+        return 1;
+    }
+    return 0;
+}
+
+const EateryList = ({navigation, eateries}) => {
+    if (eateries.length === 0){
+        return (<Text style={styles.header}>No Eateries</Text>);
+    }
+
+    eateries.sort(sortEateries);
+    const eateriesData = Object.entries(
+        eateries.reduce((obj, eatery) => {
+            if (isOpen(eatery)) {
+                if (!obj['Open']) {
+                    obj['Open'] = [];
+                }
+                obj['Open'].push(eatery);
+            }
+            else {
+                if (!obj['Closed']) {
+                    obj['Closed'] = [];
+                }
+                obj['Closed'].push(eatery);
+            }
+            return obj;
+        }, {})
+    ).map(([status, eateries]) => ({
+        data: eateries.sort(sortEateries),
+        title: status,
+    }));
+
+    console.log(eateriesData);
+    return (
+        <SafeAreaView style={styles.container}>
+            <SectionList
+                sections={eateriesData}
+                keyExtractor={(item, index) => item + index}
+                renderItem={({item}) => <EateryItem id={item.id} navigation={navigation} {...item}/>}
+                renderSectionHeader={({section: {title}}) => (
+                    <Text style={styles.header}>{title}</Text>
+                )}
+            />
+        </SafeAreaView>
+    );
+};
+
+EateryList.propTypes = {
+    navigation: PropTypes.object.isRequired,
+    eateries: PropTypes.arrayOf(
+        PropTypes.shape({
+            building: PropTypes.string.isRequired,
+            closeTime: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            openTime: PropTypes.string.isRequired,
+            uuid: PropTypes.string.isRequired,
+        }),
+    ),
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 0,
+        marginHorizontal: 0,
+    },
+    header: {
+        fontSize: 20,
+        opacity: 0.5,
+        marginHorizontal: 10,
+        marginTop: 10,
+    },
+});
+
+export default EateryList;
--- src/screens/EateryScreen.jsx
+++ src/screens/EateryScreen.jsx
@@ -2,42 +2,32 @@ import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import CustomHeader from "../components/CustomHeader";
 import * as PropTypes from "prop-types";
+import EateryList from "../components/Eatery/EateryList";
 
-const EateryScreen = ({navigation, eateries, isLoaded}) => {
-    console.log(eateries);
+const EateryScreen = ({navigation, eateries}) => {
     return (
         <View style={styles.container}>
             <CustomHeader navigation={navigation} title="Eateries">
             </CustomHeader>
-            <Text>test</Text>
+            <EateryList navigation={navigation} eateries={eateries}/>
         </View>
     );
 };
 
 EateryScreen.navigationOptions = {
-    title: 'Eateries',
     headerShown: false,
 };
 EateryScreen.propTypes = {
     navigation: PropTypes.object.isRequired,
-    /*events: PropTypes.arrayOf(
+    eateries: PropTypes.arrayOf(
         PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            description: PropTypes.string.isRequired,
-            area: PropTypes.string.isRequired,
             building: PropTypes.string.isRequired,
-            openHouse: PropTypes.string.isRequired,
-            startTime: PropTypes.string.isRequired,
-            endTime: PropTypes.string.isRequired,
-            room: PropTypes.string.isRequired,
-        })).isRequired,
-    areas: PropTypes.arrayOf(
-        PropTypes.shape({
+            closeTime: PropTypes.string.isRequired,
             name: PropTypes.string.isRequired,
+            openTime: PropTypes.string.isRequired,
             uuid: PropTypes.string.isRequired,
-            color: PropTypes.string.isRequired,
-        })
-    )*/
+        }),
+    ),
 };
 
 export default EateryScreen;
